d3c7b083e85e1a060779d3373bc769a7c99aee87
Raman Kaur <rkaulakh2986@gmail.com>
2025-03-31 10:04:28 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,21 +1,27 @@
 // Your code here...
-int main(){
+#include <stdio.h>
+int main() {
     int a[100];
-    int shifts,N;
+    int shifts, N;
     
-    scanf("%d",&N);
-    for(int i=0;i<=N-1;i++){
-        scanf("%d",&a[i]);
-    }scanf("%d",&shifts);
-    int temp;
-    for(int k=0;k<shifts;k++){
-        temp=a[0];
-    for(int i=n-1;i>=1;i++){
-        a[i-1]=a[i];
+    scanf("%d", &N);
+    for(int i = 0; i <= N-1; i++) {
+        scanf("%d", &a[i]);
     }
-    a[N-1]=temp;
-}
-for(int i=0;i<=N-1;i++){
-    printf("%d ",a[i]);
-}
+    scanf("%d", &shifts);
+    
+    int temp;
+    for(int k = 0; k < shifts; k++) {
+        temp = a[N-1];  // Store the last element
+        for(int i = N-1; i > 0; i--) {
+            a[i] = a[i-1];  // Shift elements to the right
+        }
+        a[0] = temp;  // Move the last element to the first position
+    }
+    
+    for(int i = 0; i <= N-1; i++) {
+        printf("%d ", a[i]);
+    }
+    
+    return 0;
 }
