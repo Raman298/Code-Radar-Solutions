5d1cd6be2dc09f8f0a34ad06395988f7e521416b
Raman Kaur <rkaulakh2986@gmail.com>
2025-04-10 22:19:35 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -2,7 +2,7 @@
 int duplielement(int arr[],int n){
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            if(arr[i]ar>r[j]){
+            if(arr[i]>arr[j]){
                 return arr[i];
             }
         }
