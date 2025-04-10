6b555cb15b88b9fdf9cdd1725abfab0ea6dc55ba
Raman Kaur <rkaulakh2986@gmail.com>
2025-04-10 18:57:41 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
-int max2(int arr[],int n){
-    int max=arr[0];
-      int max2=arr[0];
-      for(int i=0; i<n; i++){
-            if(arr[i]>max){
-                  max=arr[i];
-      }}
-       for(int i=0; i<n; i++){
-            if(arr[i]!=max && arr[i]>max2){
-                max2=arr[i];
-      }
-      }
-      if(max2<max){
-return max2;}
-return -1;
-}
-int main(){
-      int n;
-      scanf("%d",&n);
-      int arr[n];
-      for(int i=0; i<n; i++){
-            scanf("%d",&arr[i]);
-      }
-      printf("%d",max2(arr,n));
-}
+// #include <stdio.h>
+// int max2(int arr[],int n){
+//     int max=arr[0];
+//       int max2=arr[0];
+//       for(int i=0; i<n; i++){
+//             if(arr[i]>max){
+//                   max=arr[i];
+//       }}
+//        for(int i=0; i<n; i++){
+//             if(arr[i]!=max && arr[i]>max2){
+//                 max2=arr[i];
+//       }
+//       }
+//       if(max2<max){
+// return max2;}
+// return -1;
+// }
+// int main(){
+//       int n;
+//       scanf("%d",&n);
+//       int arr[n];
+//       for(int i=0; i<n; i++){
+//             scanf("%d",&arr[i]);
+//       }
+//       printf("%d",max2(arr,n));
+// }
 
 
 // #include <stdio.h>
@@ -68,3 +68,40 @@ int main(){
 //     printf("%d", max2);
 //     return 0;
 // }
+
+
+
+#include <stdio.h>
+#include <limits.h> // For INT_MIN
+
+int max2(int arr[], int n) {
+    if (n < 2) {
+        return -1; // Edge case: Not enough elements
+    }
+
+    int max = INT_MIN;
+    int secondMax = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            secondMax = max; // The old max becomes the new secondMax
+            max = arr[i];
+        } else if (arr[i] > secondMax && arr[i] != max) {
+            secondMax = arr[i];
+        }
+    }
+
+    // If secondMax is still INT_MIN, all elements were equal (or invalid)
+    return (secondMax != INT_MIN) ? secondMax : -1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("%d", max2(arr, n));
+    return 0;
+}
